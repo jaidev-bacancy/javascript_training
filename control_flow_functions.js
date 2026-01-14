326f810519190bf72950fa0329314ac86085e985
jaidev-21 <jaidevs2107@gmail.com>
2026-01-14 21:53:36 +0530

    Complete Assignment-1

---
+++ control_flow_functions.js
@@ -0,0 +1,8 @@
+/* Assignment - 1 : Create a program to check whether a number is positive, negative or zero. */
+let num = 1;
+// let num = 0;
+// let num = -1;
+
+if (num == 0) { console.log("The number is zero."); }
+else if (num > 0) { console.log("The number is positive."); }
+else { console.log("The number is negative."); }
