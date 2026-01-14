f22ac096758d31d0c91b39d5fba00b635cdc1638
jaidev-21 <jaidevs2107@gmail.com>
2026-01-14 22:06:00 +0530

    Complete Assignment-2

--- control_flow_functions.js
+++ control_flow_functions.js
@@ -6,3 +6,11 @@ let num = 1;
 if (num == 0) { console.log("The number is zero."); }
 else if (num > 0) { console.log("The number is positive."); }
 else { console.log("The number is negative."); }
+
+
+/* Assignment - 2 : Create a program to check whether the number in range 1 to 20 are even or odd. */
+for (let i = 1; i <= 20; i++)
+{
+    if (i & 1) { console.log(`${i} is odd.`); }
+    else { console.log(`${i} is even.`); }
+}
